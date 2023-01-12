abdba3963e8e75e4b3700c5f94757cb48d7e1cef
Eliabe Ribeiro Mota <100526198+Eliabe-Ribeiro-22@users.noreply.github.com>
2023-01-11 22:25:45 -0300

    README - Desenvolvida documentação do projeto(11/01)

---
+++ README.m
@@ -0,0 +1,18 @@
+# API-Clima WeatherCode
+
+## Descrição do projeto
+
+Aplicação totalmente didática, onde foi desenvolvido uma plataforma que faz consumo de uma **API externa**,
+que traz informações sobre o clima da cidade pesquisada pelo usuário.
+
+Nele, você digita o nome da cidade, em seguida clica no botão pesquisar
+Este projeto foi estudado no curso de Desenvolvimento Web na escola Code Universe.
+
+## Tecnologias utilizadas
+
+- React.js
+- JSX
+- Api (http://api.weatherapi.com/v1)
+- Axios
+- Css
+- JavaScript 
